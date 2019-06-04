8f089ae217fb0560293fda9b7557b4460aba9f13
FULLNAME <you@yourdomain.example.com>
2019-06-04 09:32:59 +0200

    update job

--- Fig4_BiasOpitimizer/R/job.sh
+++ Fig4_BiasOpitimizer/R/job.sh
@@ -2,8 +2,8 @@
 #PBS -l nodes=1:ppn=1
 #PBS -l mem=8gb
 #PBS -l walltime=12:00:00
-#PBS -o log/SWIFT.$PBS_JOBID
-#PBS -e log/SWIFT.$PBS_JOBID
+#PBS -o log/SWIFT.o$PBS_JOBID
+#PBS -e log/SWIFT.e$PBS_JOBID
 
 ml R/3.4.4-intel-2018a-X11-20180131; ulimit -s unlimited
 cd /kyukon/data/gent/vo/000/gvo00074/felicien/R/InvSWIFToptim/Fig4_BiasOpitimizer
