023af46b6df3d957fcff86e27cc7ea1a20132394
FULLNAME <you@yourdomain.example.com>
2019-06-11 17:34:37 +0200

    runs

---
+++ Fig4_BiasOpitimizer/runs/run_00008/script.R
@@ -0,0 +1,36 @@
+rm(list=ls())
+
+library('lhs')
+library('SWIFT')
+library('sm')
+library('ks')
+library('GenSA')
+
+setwd('../../')
+source('./R/dataprepStandardPara.R')
+source('./R/dataprepRestrictionRanges.R')
+source('./R/functionVarMatrix.R')
+source('./R/functionIsoSpace.R')
+source('./R/SWIFT.R')
+source('./R/functionPSIvariance.R')
+source('./R/functionRandomDataToIsospace.R')
+source('./R/functionLogLik.R')
+source('./R/functionOptim.R')
+
+itterations = 100
+Btrue = 0.914437141718809
+RunForWhichIsotope =  'Both'
+scenario =  4
+run_id =  8
+
+folder <-paste0('./runs/run_',sprintf('%05i',run_id))
+setwd(folder)
+FD_file <- list.files(getwd(),pattern = 'FD_*')
+FD <- read.csv(file.path(getwd(),FD_file))
+
+setwd('../../')
+Scenario <- switch(scenario,'Sc1','Sc2','Sc3','Sc4')
+B_Sc1 <- optim(par=0.95, fn=LogLikOptim, lower = 0.905, upper = 0.995, method = 'Brent')
+
+DF <- data.frame(Btrue=Btrue,Bsc=B_Sc1$par)
+write.csv(DF, file = file.path(getwd(),paste0('./runs/run_',sprintf('%05i',run_id)),'results.csv'))
